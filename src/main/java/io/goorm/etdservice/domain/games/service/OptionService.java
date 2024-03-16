93b1a8972e971dc87e36bdd4c2b30cb85ec8905d
mk1-p <mk1.dev662@gmail.com>
2024-03-16 19:34:23 +0900

    CLN: add comment

--- src/main/java/io/goorm/etdservice/domain/games/service/OptionService.java
+++ src/main/java/io/goorm/etdservice/domain/games/service/OptionService.java
@@ -8,5 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+// 클래스 명칭 고민해보기.
 public class OptionService {
+
+
+    // 게임 서버 생성 옵션 적용 시
+    // 기본 게임 환경 옵션 자동 생성
+    // 서버 Pod 을 만들기 위한 데이터 폼을 만들어야 하는가??
+
 }

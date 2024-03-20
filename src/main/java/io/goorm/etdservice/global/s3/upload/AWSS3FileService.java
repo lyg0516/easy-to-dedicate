e282506cdda9ba84041ae0961a4a20b87d6ed7e8
lyg0516 <dudrms9397@naver.com>
2024-03-20 21:00:46 +0900

    add AwsS3Constant

--- src/main/java/io/goorm/etdservice/global/s3/upload/AWSS3FileService.java
+++ src/main/java/io/goorm/etdservice/global/s3/upload/AWSS3FileService.java
@@ -2,8 +2,11 @@ package io.goorm.etdservice.global.s3.upload;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import io.goorm.etdservice.global.exception.DomainException;
+import io.goorm.etdservice.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,11 +21,14 @@ public class AWSS3FileService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public void putObject(MultipartFile file,String prefix ,String filename) throws IOException {
+    public void putObject(MultipartFile file,String key ,String filename) throws DomainException {
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(file.getContentType());
         objectMetadata.setContentLength(file.getSize());
-        // TODO exception 처리
-        amazonS3Client.putObject(bucket, prefix + "/" + filename, file.getInputStream(), objectMetadata);
+        try {
+            amazonS3Client.putObject(bucket, key+ "/" + filename, file.getInputStream(), objectMetadata);
+        } catch (IOException e) {
+            throw new DomainException(ErrorCode.INVALID_IMAGE_FILE);
+        }
     }
 }

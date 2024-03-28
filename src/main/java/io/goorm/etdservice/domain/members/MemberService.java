56d2c89872efaf298eb4052e5dbf19551f90bd70
mk1-p <mk1.dev662@gmail.com>
2024-03-28 19:22:29 +0900

    ENH: add UserPrincipal extend UserDetial

--- src/main/java/io/goorm/etdservice/domain/members/MemberService.java
+++ src/main/java/io/goorm/etdservice/domain/members/MemberService.java
@@ -1,15 +1,21 @@
 package io.goorm.etdservice.domain.members;
 
 
+import io.goorm.etdservice.global.exception.ErrorCode;
 import io.goorm.etdservice.global.security.OAuth2Attribute;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -26,4 +32,12 @@ public class MemberService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UUID memberId = UUID.fromString(username);
+        //TODO 추후 캐싱 기능 추가 - 매번 API 조회떄마다 멤버를 DB에서 조회하는 것이 비효율적
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.NOT_FOUND_DATA.getDetail()));
+        return UserPrincipal.create(member);
+    }
 }

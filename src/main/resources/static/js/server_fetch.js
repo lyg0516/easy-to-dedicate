046169b4b12a6eceb373174cd9db57187807d01e
mk1-p <mk1.dev662@gmail.com>
2024-04-07 20:19:05 +0900

    CLN: add update game option logic

--- src/main/resources/static/js/server_fetch.js
+++ src/main/resources/static/js/server_fetch.js
@@ -25,9 +25,10 @@ export async function createServer(option) {
     const endpoint = `${PREFIX_URI}`;
 
     //TODO 요청 후 결과 반환
-    return await commApi.fetchApi(endpoint,'POST',option);
-
+    await commApi.fetchApi(endpoint,'POST',option);
+    window.alert(`서버 생성이 완료되었습니다!`);
     //TODO 추가 로직
+
 }
 
 export async function restartServer(serverId) {

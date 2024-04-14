a01525b6745c441e37657d61297c1ed098acf97e
lyg0516 <dudrms9397@naver.com>
2024-04-14 19:25:49 +0900

    add image

--- src/main/java/io/goorm/etdservice/domain/servers/types/ControlType.java
+++ src/main/java/io/goorm/etdservice/domain/servers/types/ControlType.java
@@ -2,8 +2,19 @@ package io.goorm.etdservice.domain.servers.types;
 
 public enum ControlType {
 
-    CREATE,
-    RESTART,
-    STOP,
+    CREATE("create"),
+    RESTART("restart"),
+    STOP("stop"),
+    DELETE("delete");
+
+    private final String label;
+
+    ControlType(String label){
+        this.label = label;
+    }
+
+    public String label(){
+        return label;
+    }
 
 }

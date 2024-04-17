221f490d331f0a0eea3757d669dab6e6aa2f029c
mk1-p <mk1.dev662@gmail.com>
2024-04-17 23:30:46 +0900

    FIX: add repository anno

--- src/main/java/io/goorm/etdservice/domain/games/entity/GameOptionRepository.java
+++ src/main/java/io/goorm/etdservice/domain/games/entity/GameOptionRepository.java
@@ -1,9 +1,11 @@
 package io.goorm.etdservice.domain.games.entity;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface GameOptionRepository extends MongoRepository<GameOption, UUID> {
 
 }

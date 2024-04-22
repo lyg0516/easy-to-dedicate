12b81cc69e4d2a57b15035845bbd8514dcbb5a9e
lyg0516 <dudrms9397@naver.com>
2024-04-22 23:48:25 +0900

    fix:message

--- src/main/java/io/goorm/etdservice/domain/games/entity/EnshroudedOptionMessage.java
+++ src/main/java/io/goorm/etdservice/domain/games/entity/EnshroudedOptionMessage.java
@@ -1,9 +1,20 @@
 package io.goorm.etdservice.domain.games.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy.class)
 public class EnshroudedOptionMessage {
 
 

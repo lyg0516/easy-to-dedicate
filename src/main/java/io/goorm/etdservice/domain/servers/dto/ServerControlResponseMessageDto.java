1a54ebb1d94f751af9fe26f00004db8b6e827887
lyg0516 <dudrms9397@naver.com>
2024-04-23 08:31:49 +0900

    fix: messae listener

--- src/main/java/io/goorm/etdservice/domain/servers/dto/ServerControlResponseMessageDto.java
+++ src/main/java/io/goorm/etdservice/domain/servers/dto/ServerControlResponseMessageDto.java
@@ -1,5 +1,6 @@
 package io.goorm.etdservice.domain.servers.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,12 @@ import java.util.UUID;
 public class ServerControlResponseMessageDto {
 
     private String type;
+
+    @JsonProperty("server_id")
     private UUID serverId;
     private String status;
+
+    @JsonProperty("server_control_id")
     private Long serverControlId;
     private String host;
     private Integer port;
@@ -29,5 +34,18 @@ public class ServerControlResponseMessageDto {
         this.host = host;
         this.port = port;
         this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerControlResponseMessageDto{" +
+                "type='" + type + '\'' +
+                ", serverId=" + serverId +
+                ", status='" + status + '\'' +
+                ", serverControlId=" + serverControlId +
+                ", host='" + host + '\'' +
+                ", port=" + port +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
